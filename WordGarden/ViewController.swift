149d996cfaf871307157de56b902c4d0bbc7c026
Louise Kim <kimemu@bc.edu>
2021-02-15 14:17:16 -0500

    3.3 Working with Keyboard, Dealing with the first responder, and Handling UITextField Actions

--- WordGarden/ViewController.swift
+++ WordGarden/ViewController.swift
@@ -25,11 +25,27 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let text = guessedLetterTextField.text!
+        guessLetterButton.isEnabled = !(text.isEmpty)
     }
-
+    
+    func updateUIAfterGuess() {
+        guessedLetterTextField.resignFirstResponder()
+        guessedLetterTextField.text! = ""
+        guessLetterButton.isEnabled = false
+    }
+    @IBAction func guessedLetterFieldChanged(_ sender: UITextField) {
+        let text = guessedLetterTextField.text!
+        guessLetterButton.isEnabled = !(text.isEmpty)
+        
+    }
+    
+    @IBAction func doneKeyPressed(_ sender: UITextField) {
+         updateUIAfterGuess()
+    }
     
     @IBAction func guessLetterButtonPressed(_ sender: UIButton) {
+        updateUIAfterGuess()
     }
     
     @IBAction func playAgainButtonPressed(_ sender: UIButton) {
